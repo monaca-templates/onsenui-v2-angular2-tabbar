0eff289269fc7a04cd522c0a266f7cdb0fce8ae3
Naoki Matagawa <naoki.m@asial.co.jp>
2016-09-17 06:48:51 +0900

    [incomplete] Updated template to support Onsen UI 2.0.0 and Angular 2.0.0 and angular2-onsenui 1.0.0-rc.1.

--- src/app/first.ts
+++ src/app/first.ts
@@ -1,9 +1,7 @@
 import {Component} from '@angular/core';
-import {ONS_DIRECTIVES} from 'angular2-onsenui';
 
 @Component({
   selector: 'ons-page',
-  directives: [ONS_DIRECTIVES],
   template: `
     <div class="content">
       <p>I am the first tab.</p>
