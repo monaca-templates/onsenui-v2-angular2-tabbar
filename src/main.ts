c3db5f934969bbf2bedae949a7efa72567477b9a
Yong Sopheaktra <yong@asial.co.jp>
2018-09-18 18:27:26 +0900

    Webpack4 templates with build deps (#9)
    * webpack 4 configuration
    
    * update monaca npm script
    
    * update monaca:preview
    
    * add loader.js
    
    * set host to MONACA_SERVER_HOST environment variable on Monaca Cloud IDE
    
    * minor fix - refine host and port param
    
    * remove template_type and build from project_info.json
    
    * bump version to 3.0.0

--- src/main.ts
+++ src/main.ts
@@ -40,5 +40,7 @@ if (process.env.NODE_ENV === 'production') {
 })
 class AppModule {}
 
+if (module['hot']) module['hot'].accept();
+
 platformBrowserDynamic().bootstrapModule(AppModule)
 .catch(err => console.error(err));
